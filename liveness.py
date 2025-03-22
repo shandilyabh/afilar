669499f5fd9c07a2654b77790b96785dbc9bc20b
Abhishek Shandilya <abhi170203@gmail.com>
2025-03-22 23:02:06 +0530

    experimenting with cv2

---
+++ liveness.py
@@ -0,0 +1,24 @@
+"""
+test code to check if liveness detection works
+"""
+import cv2 #type: ignore
+
+cap = cv2.VideoCapture(0)  # Open webcam
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to access the webcam.")
+        break
+
+    cv2.imshow("Webcam Feed - Press 'C' to Capture, 'Q' to Quit", frame)
+
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('c'):
+        cv2.imwrite("my_face.jpg", frame)
+        print("Image saved as my_face.jpg")
+    elif key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
